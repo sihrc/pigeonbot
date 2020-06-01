ff87e46f2aed91947cca84dfc57a059cbb6e6380
Chris Lee <sihrc.c.lee@gmail.com>
2020-05-31 20:49:15 -0400

    update wip

--- pigeonbot/main.py
+++ pigeonbot/main.py
@@ -1,37 +1,74 @@
+import asyncio
+import atexit
+import json
+import logging
 import os
 
+from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+
 import discord
-import asyncio
 
 CLIENT = discord.Client()
-PREVIOUS_MESSAGES = [ None ]
+logging.basicConfig()
+
+# Dumb way to save state
+servers_path = Path("servers.cfg")
+servers = defaultdict(list)
+if servers_path.exists():
+    with servers_path.open("r") as f:
+        servers.update(json.loads(f))
+
+
+atexit.register(lambda: json.dump(servers, servers_path.open("w")))
+
+
 @CLIENT.event
 @asyncio.coroutine
 def on_ready():
-    print("PigeonBot is online")
+    logging.info("PigeonBot is online")
+
+
+SEND_MESSAGE_EXECUTOR = ThreadPoolExecutor(20)
+
+
+async def send_message(client, dst_ch, content):
+    return CLIENT.send_message(CLIENT.get_channel(dst_ch), content)
+
 
 @CLIENT.event
-@asyncio.coroutine
-def on_message(message):
-    if message.server.name.startswith("IHANA") and \
-               message.channel.name == "live_calls" and \
-               message.author.name == "IHA BDO Bot":
-       print("Relaying message.")
+async def on_message(message):
+    if message.content.startswith("^p subscribe"):
+        try:
+            src_ch_id, dst_ch_id = message.content.split()[2:4]
+        except:
+            await send_message(CLIENT, message.channel.id, "LOL that didn't work")
+        else:
+            listening = servers[src_ch_id]
+            if dst_ch_id not in listening:
+                listening.append(dst_ch_id)
+    elif message.content.startswith("^p unsubscribe"):
+        try:
+            src_ch_id, dst_ch_id = message.content.split()[2:4]
+        except:
+            await send_message(CLIENT, message.channel.id, "LOL that didn't work")
+        else:
+            listening = servers[src_ch_id]
+            if dst_ch_id in listening:
+                listening.remove(dst_ch_id)
+    else:
+        listening = servers.get(message.channel.id, [])
+        await asyncio.gather(
+            *(send_message(CLIENT, dst_ch, message.content) for dst_ch in listening)
+        )
 
-       if PREVIOUS_MESSAGES[0] is not None:
-           try:
-               yield from CLIENT.delete_message(PREVIOUS_MESSAGES[0])
-           finally:
-               PREVIOUS_MESSAGES[0] = None
-
-
-       message = yield from CLIENT.send_message(CLIENT.get_channel("254051680222445568"), message.content)
-       PREVIOUS_MESSAGES[0] = message
 
 @CLIENT.event
 @asyncio.coroutine
 def on_server_join(server):
-    print(server)
+    logging.info(server)
+
 
 if __name__ == "__main__":
     CLIENT.run(os.getenv("email"), os.getenv("password"))
--- setup.py
+++ setup.py
@@ -2,21 +2,14 @@
 import os
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
 setup(
     name="pigeonbot",
-    version='0.1',
-    description='Pigeon Relay Bot',
-    author='Chris Lee',
-    author_email='chrisklee93@gmail.com',
-    url='chrisklee.me',
+    version="0.1",
+    description="Pigeon Relay Bot",
+    author="Chris Lee",
+    author_email="chrisklee93@gmail.com",
+    url="chrisklee.me",
     packages=find_packages(),
-    install_requires=[
-        str(item.req) for item in
-        parse_requirements(os.path.join(
-                os.path.dirname(__file__),
-                "requirements.txt"
-        ), session=False)
-    ]
+    install_requires=["discord==1.0.1"],
 )
